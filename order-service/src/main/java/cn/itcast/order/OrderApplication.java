e2ee288ad0352ff60de22bcc1be43863cd46587b
jy <78412709@qq.com>
2022-11-13 19:33:37 +0800

    nacos配置管理，feign远程调用

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,14 +1,18 @@
 package cn.itcast.order;
 
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.config.DefaultFeignConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
+@EnableFeignClients(clients = UserClient.class, defaultConfiguration = DefaultFeignConfiguration.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
@@ -17,11 +21,12 @@ public class OrderApplication {
 
     /**
      * 创建RestTemplate并注入spring容器
+     *
      * @return
      */
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
